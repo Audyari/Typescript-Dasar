8d07f95b59397d8708c3214589ed64a2c8cb7347
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 12:41:09 +0700

    1. Say Hello Function

---
+++ tests/hello.test.ts
@@ -0,0 +1,12 @@
+
+describe('Bikin Hello World', () => {
+    it('should return Hello World', () => {
+        expect('Hello World').toBe('Hello World');
+    });
+})
+
+describe('Bikin Hello {name}', () => {
+    it('should return Hello {name}', () => {
+        expect('Hello {name}').toBe('Hello {name}');
+    });
+})
