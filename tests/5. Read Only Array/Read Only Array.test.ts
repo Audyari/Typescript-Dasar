a207bff18f10b193d684132c7e9a2ca4202c2d5f
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 14:22:12 +0700

    5. Read Only Array

---
+++ tests/5. Read Only Array/Read Only Array.test.ts
@@ -0,0 +1,12 @@
+describe("Read Only Array", () => {
+  it("Read Only Array", () => {
+    let Hobi: ReadonlyArray<string> = ["Sepakbola", "Basket", "Voli"];
+
+    console.info(Hobi);
+    console.info(Hobi[0]);
+    console.info(Hobi[1]);
+    console.info(Hobi[2]);
+
+
+  });
+});
