03c69fb485fb11726629651da4b53f69e433c366
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 14:54:40 +0700

    8. Union Type

---
+++ tests/8. Union Type/Union Type.test.ts
@@ -0,0 +1,13 @@
+describe("Union Type", function () {
+  it("should Union Type", function () {
+    let sample: number | string | boolean = 123;
+
+    sample = "123";
+
+    sample = true;
+
+    sample = "Audyari Wiyono";
+
+    console.info(sample);
+  });
+});
