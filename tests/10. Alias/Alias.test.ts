5d85e52e65743e71d7a8c92281cd45dec13d6dcf
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 15:50:04 +0700

    10. Alias

---
+++ tests/10. Alias/Alias.test.ts
@@ -0,0 +1,20 @@
+import { Category, Product } from "../../src/10. Alias/Alias";
+
+describe("Alias", function () {
+  it("Alias", function () {
+    const category: Category = {
+      id: 1,
+      name: "Electronics",
+    };
+
+    const product: Product = {
+      id: 2,
+      name: "Laptop",
+      price: 15000000,
+      category: category,
+    };
+
+    console.info(category);
+    console.info(product);
+  });
+});
