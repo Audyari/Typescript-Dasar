4dd541ec7803fe4b8f71b8e53748742db727029a
Audyari Wiyono <audy123ari@gmail.com>
2024-07-31 20:28:46 +0700

    33. If Statement

---
+++ tests/33. If Statement/If Statement.test.ts
@@ -0,0 +1,18 @@
+
+describe('33. If Statement', () => {
+    it('33. If Statement', () => {
+        
+        const examValue = 90;
+
+        if (examValue > 80) {
+            console.info('Good');
+        } else if (examValue > 60) {
+            console.info('Not Bad');
+        } else {
+            console.info('Try Again');
+        }
+    })
+})
+
+
+
